cce864ed1bcbcf7e4726fb1e59efa4d606a9501a
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2025-11-27 17:42:08 +0000

    Add methods to insert an action before/after another
    The idea being, it's nice to avoid indices where possible to maximize compatibility

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -3,7 +3,9 @@ using BepInEx;
 namespace Silksong.FsmUtil;
 
 [BepInAutoPlugin("org.silksong-modding.fsmutil")]
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 public partial class Plugin : BaseUnityPlugin
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
 {
     private void Awake()
     {
